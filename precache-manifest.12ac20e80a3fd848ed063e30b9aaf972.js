7b5b83dd1243b0f50be5bf215f65ae77d52573f9
Ismail Al Ghani <ismailalghani@gmail.com>
2020-09-15 06:49:06 +0700

    Updates

---
+++ precache-manifest.12ac20e80a3fd848ed063e30b9aaf972.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bf3c058cbc315d07f75729b9964a98",
+    "url": "/pajakme/index.html"
+  },
+  {
+    "revision": "25099ae560f1e28c4511",
+    "url": "/pajakme/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0bc7b9ae0f887f0b1ca1",
+    "url": "/pajakme/static/css/main.4d390909.chunk.css"
+  },
+  {
+    "revision": "25099ae560f1e28c4511",
+    "url": "/pajakme/static/js/2.1e398531.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pajakme/static/js/2.1e398531.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bc7b9ae0f887f0b1ca1",
+    "url": "/pajakme/static/js/main.d93cb1af.chunk.js"
+  },
+  {
+    "revision": "51d798032981ffd53402",
+    "url": "/pajakme/static/js/runtime-main.0272b389.js"
+  },
+  {
+    "revision": "04c15e3afee8caf90e68a100abad89e7",
+    "url": "/pajakme/static/media/icons8-bootstrap.04c15e3a.svg"
+  },
+  {
+    "revision": "b5d6376a00dc483836f12b3132a7d3f8",
+    "url": "/pajakme/static/media/icons8-github.b5d6376a.svg"
+  },
+  {
+    "revision": "5a1dec63599121f062f1743400392d65",
+    "url": "/pajakme/static/media/icons8-instagram.5a1dec63.svg"
+  },
+  {
+    "revision": "f2fa6b25297c0cccb90eb88949fff08d",
+    "url": "/pajakme/static/media/icons8-linkedin.f2fa6b25.svg"
+  },
+  {
+    "revision": "698be2f2c66501058d474b97478ca62b",
+    "url": "/pajakme/static/media/icons8-react.698be2f2.svg"
+  },
+  {
+    "revision": "d52f7b4fcefeb1f5e1aaa02dd0fa65af",
+    "url": "/pajakme/static/media/icons8-redux.d52f7b4f.svg"
+  },
+  {
+    "revision": "1023f78a2492cb3e48519909df2f1919",
+    "url": "/pajakme/static/media/icons8-sass.1023f78a.svg"
+  },
+  {
+    "revision": "7a9cdbce9915e7e3111812e0c5ac4304",
+    "url": "/pajakme/static/media/logo_transparent_1.7a9cdbce.png"
+  }
+]);
